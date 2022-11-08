4cd9535c252b019316002bb06f2de401e09e3c6d
Aaron Collins <aa.collins.95@gmail.com>
2022-11-08 14:44:32 -0500

    solution gets answers

--- staircase/foo.py
+++ staircase/foo.py
@@ -1,5 +1,37 @@
-def f(x, y ,z):
-    print (x + y + z)
+import math
 
-f(*[1,2])
+CACHE = []
+def main(n):
+    CACHE = [['-' for c in range(n+1)] for r in range(n+1)]
+    maxS = math.floor(((8 * n  + 1) ** 0.5 - 1) / 2)
+    for k in range(int(maxS)):
+        Q(n, k)
+    print CACHE
+    #return Q(n ,maxS)
+"""
+def Q(n, k):
+    if n == k == 1:
+        return 1
+    elif n > 0 and k == 0:
+        return 0
+    elif n < k or k < 1:
+        return 0
+    else:
+        return Q(n - k, k) + Q(n - k, k-1)
+"""
 
+def Q(n, k):
+    if n == k == 1:
+        return 1
+    elif n > 0 and k == 0:
+        return 0
+    elif n < k or k < 1:
+        return 0
+    else:
+        print("n",n,"k",k)
+        print('len of CACHE', len(CACHE))
+        if CACHE[n][k] == "-":
+            CACHE[n][k] = Q(n - k, k) + Q(n - k, k-1)
+        return CACHE[n][k]
+
+main(13)
--- staircase/solution_alpha.py
+++ staircase/solution_alpha.py
@@ -1,38 +1,79 @@
 import sys
 import timeit
+import math
 from solution import *
 
-def solution_alpha(n):
-    #another implementation with dynamic programming
-    comboRef = [[-1],[-1],[-1]]
-    for b in range(3, n + 1):
-        maxStairs = get_max_stairs(b)
-        comboRef.append([-1] * (maxStairs + 1))
+class StaircaseSolver:
+    """
+    A class for helping facilitate testing. Takes a list of tuples and a method.
+    Tests that the use cases evaluate properly on the provided method
 
-        for s in range(2, maxStairs + 1):
-            if s == 2:
-                print(b,s)
-                comboRef[b][s] = (b - 1) / 2 #formula for number of pairings
-            else:
-                combinationsAtStaircaseLength(b, s, comboRef)
-    showComboRef(comboRef)
-    return sum(comboRef[n][2:])
+    Args
+    method - the method from solution.py that is being tested against the use cases
+    use_cases - a list of tuples where tuple[1] is the expected result of method(tuple[0])
+    (ie, tuple = (method_input, output))
 
-def combinationsAtStaircaseLength(b, s, comboRef):
-    #recursive function to calculate cominations at lengths
-    minTailSum = get_minimum_tail_sum(s-1) #3
-    combos = 0
-    for tailSum in range(minTailSum, b):
-        head = b - tailSum
-        if head > tailSum:
-            #comboRef[b][s] = comboRef[tailSum][s-1]
-            combos += comboRef[tailSum][s-1]
-        elif head == tailSum:
-            
-    comboRef[b][s] = combos
+    Returns
+    Nothing. If there is an assertion error, print the input, expected result, and actual result.
+    If there are no errors, print('Method [method] valid for all provided use cases')
+    """
+    def __init__(self, bricks):
+        """
+        Stores provided values as class variables and begins testing
+        """
+        self.cache = self.generate_cache(bricks + 1)
+        self.solution = self.get_staircases(bricks)
+    
+    def get_solution(self):
+        return self.solution
 
-def showComboRef(comboRef):
-    for i in range(len(comboRef)):
-        print(i, comboRef[i])
+    def generate_cache(self,n):
+        cache = [[None for c in range(n)] for r in range(n)]
+        return cache
 
-solution_alpha(10)
+    def show_2d_list(self, data):
+        for d in data:
+            print d
+
+    def get_staircases(self, n):
+        maximum_stair_len = int(math.floor(((8 * n + 1) ** 0.5 - 1) / 2)) #Comes from OEIS A008289
+        print maximum_stair_len
+        for k in range(1, int(maximum_stair_len) + 1):
+            self.partition_function_q(n, k)
+        print self.cache[-1][1:maximum_stair_len]
+        return sum(self.cache[-1][1:maximum_stair_len])
+        self.show_2d_list(self.cache)
+        pass
+
+    def partition_function_q(self, n, size):
+        """
+        This is the partition function Q as per the documentation in OEIS A008289. Calculates how many
+        partitions exist for a number 'n' of a particular size 'size'
+
+        Args:
+        n - The integer value that must be broken into partitions
+        size - the size of the partitions that will be counted
+        cache - the record of previous results. The cache allows the solution to utilize dynamic programming
+
+        Returns:
+        Number of partitions of 'n' with 'size' integers
+        """
+        #most of this switch statement comes from the formula on OEIS A008289
+        if n == size == 1:
+            return 1
+        elif n > 0 and size == 0:
+            return 0
+        elif n < size or size < 1:
+            return 0
+        if self.cache[n][size] == None:
+            self.cache[n][size] = self.partition_function_q(n - size, size) + self.partition_function_q(n - size, size - 1)
+        return self.cache[n][size]
+
+def solution(n):
+    """
+    Solves problem of the staircase. Calculates the maxStairLen based on n and initiates recursion
+    """
+    return StaircaseSolver(n).get_solution()
+
+
+print solution(200)
